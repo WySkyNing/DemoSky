3095e059527d91c6f3aa6c00bca55b43d4c7c6c2
WySkyNing <ningwy1314@163.com>
2016-06-20 18:28:17 +0800

    listview

--- app/src/main/java/com/ning/demosky/view/listview/ListViewActivity.java
+++ app/src/main/java/com/ning/demosky/view/listview/ListViewActivity.java
@@ -8,9 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ning.demosky.R;
 
@@ -41,9 +43,24 @@ public class ListViewActivity extends AppCompatActivity {
 
         for (int i = 0; i < 100; i++) {
             DataBean dataBean = new DataBean();
-            dataBean.setNumber(i);
+
+            if (i <= 30){
+                dataBean.setNumber(1);
+            }else if (i <= 60){
+                dataBean.setNumber(2);
+            }else {
+                dataBean.setNumber(3);
+            }
+
+            if (i % 2 == 0){
+                dataBean.is = true;
+            }else {
+                dataBean.is = false;
+            }
+
             dataBean.setData("数据" + i);
             datas.add(dataBean);
+
         }
     }
 
@@ -78,7 +95,7 @@ public class ListViewActivity extends AppCompatActivity {
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
 
             ViewHolder viewHolder = null;
 
@@ -92,13 +109,62 @@ public class ListViewActivity extends AppCompatActivity {
 
             viewHolder.textView.setText(datas.get(position).getData());
 
-            if (datas.get(position).getNumber() % 2 != 0) {
+            if (datas.get(position).getNumber() == 1){
+
+                viewHolder.btn2.setVisibility(View.VISIBLE);
+
+                viewHolder.imageView.setVisibility(View.GONE);
+                viewHolder.btn1.setVisibility(View.GONE);
+                viewHolder.text2.setVisibility(View.GONE);
+
+            }else if (datas.get(position).getNumber() == 2){
 
                 viewHolder.imageView.setVisibility(View.VISIBLE);
-            } else {
-                Log.i("list_view_wy", position + "——");
-                viewHolder.imageView.setVisibility(View.GONE);
+                viewHolder.text2.setVisibility(View.VISIBLE);
+                viewHolder.btn2.setVisibility(View.VISIBLE);
+
+                viewHolder.btn1.setVisibility(View.GONE);
+
+
+                viewHolder.imageView.setImageResource(R.mipmap.ic_launcher);
+
+            }else if (datas.get(position).getNumber() == 3){
+
+                viewHolder.imageView.setVisibility(View.VISIBLE);
+                viewHolder.text2.setVisibility(View.VISIBLE);
+                viewHolder.btn2.setVisibility(View.VISIBLE);
+                viewHolder.btn1.setVisibility(View.VISIBLE);
+
+                viewHolder.imageView.setImageResource(R.drawable.explode_0);
+
             }
+
+            if (datas.get(position).is){
+                viewHolder.duiuhao.setVisibility(View.VISIBLE);
+            }else {
+                viewHolder.duiuhao.setVisibility(View.GONE);
+            }
+
+            viewHolder.textView.setText(datas.get(position).getData());
+            viewHolder.text1.setText("text    1");
+            viewHolder.text2.setText("text    2");
+            viewHolder.text3.setText("text    3");
+
+
+            viewHolder.btn1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(ListViewActivity.this, "btn1  :" + position, Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            viewHolder.btn2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(ListViewActivity.this, "btn2  :" + position, Toast.LENGTH_SHORT).show();
+                }
+            });
+
 
             return convertView;
         }
@@ -107,11 +173,22 @@ public class ListViewActivity extends AppCompatActivity {
     private class ViewHolder {
 
         private TextView textView;
-        private ImageView imageView;
+        private ImageView imageView ,duiuhao;
+
+        private Button btn1,btn2;
+        private TextView text1,text2,text3;
 
         public ViewHolder(View view) {
             textView = (TextView) view.findViewById(R.id.list_view_item_text_view);
             imageView = (ImageView) view.findViewById(R.id.list_view_item_image_view);
+            duiuhao = (ImageView) view.findViewById(R.id.list_view_item_image_duihao);
+
+            btn1 = (Button) view.findViewById(R.id.list_view_item_btn1);
+            btn2 = (Button) view.findViewById(R.id.list_view_item_btn2);
+
+            text1 = (TextView) view.findViewById(R.id.list_view_item_text1);
+            text2 = (TextView) view.findViewById(R.id.list_view_item_text2);
+            text3 = (TextView) view.findViewById(R.id.list_view_item_text3);
         }
     }
 
@@ -119,6 +196,8 @@ public class ListViewActivity extends AppCompatActivity {
     private class DataBean {
         private int number;
         private String data;
+
+        private boolean is;
 
         public int getNumber() {
             return number;
@@ -134,6 +213,14 @@ public class ListViewActivity extends AppCompatActivity {
 
         public void setData(String data) {
             this.data = data;
+        }
+
+        public boolean is() {
+            return is;
+        }
+
+        public void setIs(boolean is) {
+            this.is = is;
         }
     }
 }

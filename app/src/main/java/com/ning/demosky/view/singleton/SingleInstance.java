e1e1d039975003ed86c508ef4466522cd9ec451a
WySkyNing <ningwy1314@163.com>
2016-06-21 13:07:47 +0800

    SingleInstance

---
+++ app/src/main/java/com/ning/demosky/view/singleton/SingleInstance.java
@@ -0,0 +1,40 @@
+package com.ning.demosky.view.singleton;
+
+/**
+ * Created by yorki on 2016/6/21.
+ */
+public class SingleInstance {
+
+    private static volatile SingleInstance instance = null;
+
+    public static SingleInstance getInstance(){
+
+        SingleInstance _instance = instance;
+
+        if (null == instance){
+
+            synchronized (SingleInstance.class){
+
+                _instance = instance;
+
+                if (null == _instance){
+
+                    _instance = new SingleInstance();
+
+                    instance  = _instance;
+                }
+            }
+        }
+
+        return _instance;
+    }
+
+
+    private static class SingleHolder{
+        private static final SingleInstance instance = new SingleInstance();
+    }
+
+    public static SingleInstance _getInstance(){
+        return SingleHolder.instance;
+    }
+}
